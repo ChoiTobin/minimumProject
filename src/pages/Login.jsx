c6c35c551c255738454bcaca56f421f4e3f5770c
Minhyeonhong <mhh2029@gmail.com>
2022-10-21 18:05:01 +0900

    레이아웃 나눔

---
+++ src/pages/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from "styled-components";
+import Layout from '../components/Layout';
+
+const Login = () => {
+    return (
+        <Layout>
+            <div>login</div>
+        </Layout>
+    );
+};
+
+export default Login;
+
---
+++ src/pages/Write.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from "styled-components";
+import Layout from '../components/Layout';
+
+const Write = () => {
+    return (
+        <Layout>
+            <div>Write</div>
+        </Layout>
+    );
+};
+
+export default Write;
+
